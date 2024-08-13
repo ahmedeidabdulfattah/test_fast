560c91dfa58fcea06020880cb6acc3cd38df644c
ahmedeidabdulfattah <102394292+ahmedeidabdulfattah@users.noreply.github.com>
2024-08-13 04:57:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+import os
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
